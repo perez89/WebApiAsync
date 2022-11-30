c09f7435f0b6bd1be86b4e3937aeea090aba8094
Luis Paulo Lemos Perez <lplperez@hotmail.com>
2022-11-30 10:11:10 +0000

    add unit tests add health check, and custom health check

---
+++ WebApiAsync/HealthCustom/SampleHealthCheck.cs
@@ -0,0 +1,22 @@
+﻿namespace WebApiAsync.HealthCustom;
+
+public class SampleHealthCheck : IHealthCheck
+{
+    public Task<HealthCheckResult> CheckHealthAsync(
+        HealthCheckContext context, CancellationToken cancellationToken = default)
+    {
+        var isHealthy = true;
+
+        //get memory usage
+
+        if (isHealthy)
+        {
+            return Task.FromResult(
+                HealthCheckResult.Healthy("A healthy-sample result."));
+        }
+
+        return Task.FromResult(
+            new HealthCheckResult(
+                context.Registration.FailureStatus, "An unhealthy-sample result."));
+    }
+}
--- WebApiAsync/Program.cs
+++ WebApiAsync/Program.cs
@@ -1,3 +1,8 @@
+using Microsoft.AspNetCore.Diagnostics.HealthChecks;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.Diagnostics.HealthChecks;
+using WebApiAsync.HealthCustom;
+
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
@@ -6,6 +11,13 @@ builder.Services.AddControllers();
 // Learn more about configuring Swagger/OpenAPI at https://aka.ms/aspnetcore/swashbuckle
 builder.Services.AddEndpointsApiExplorer();
 builder.Services.AddSwaggerGen();
+//builder.Services.AddHealthChecks();
+
+builder.Services.AddHealthChecks()
+    .AddCheck<SampleHealthCheck>(
+        "Sample",
+        failureStatus: HealthStatus.Degraded,
+        tags: new[] { "example_health_check" });
 
 var app = builder.Build();
 
@@ -15,11 +27,31 @@ if (app.Environment.IsDevelopment())
     app.UseSwagger();
     app.UseSwaggerUI();
 }
+app
+    .UseHsts()
+    .UseHttpsRedirection()
+    .UseAuthentication()
+    .UseRouting();
 
 app.UseHttpsRedirection();
 
 app.UseAuthorization();
 
 app.MapControllers();
+
+app.MapHealthChecks("/health", new HealthCheckOptions
+{
+    ResultStatusCodes =
+    {
+        [HealthStatus.Healthy] = StatusCodes.Status200OK,
+        [HealthStatus.Degraded] = StatusCodes.Status200OK,
+        [HealthStatus.Unhealthy] = StatusCodes.Status503ServiceUnavailable
+    }
+});
+
+app.MapHealthChecks("/health-sample", new HealthCheckOptions
+{
+    Predicate = healthCheck => healthCheck.Tags.Contains("example_health_check")
+});
 
 app.Run();
